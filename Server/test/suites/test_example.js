f3517c09f2e0948cdd509f9871c3b79d836465c8
Alexander Osman <fredyalexanderosmanramirez@gmail.com>
2022-01-25 23:50:39 -0500

    new changes

--- Server/test/suites/test_example.js
+++ Server/test/suites/test_example.js
@@ -6,10 +6,10 @@ fixture`Ejemplo page Object Model`
  test.only('Inicio de sesion con gmail para completar formulario', async t => {
     await t
     .wait(1000)   
-    .typeText(pagelogin.userName, 'alexosman0512@gmail.com') // Ingresar Usuario
+    .typeText(pagelogin.userName, 'alexoman0512@gmail.com') // Ingresar Usuario
     .wait(1000)   
     .typeText(pagelogin.password, '123') // Ingresar contraseña
     .wait(1000)
     .click(pagelogin.submitUser)
-    .wait(1000) 
+    .wait(7000) 
  });
